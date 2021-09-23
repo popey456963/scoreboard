7d32e2b30b96a0fe99a593e7f035c4d571b6daf3
Alexander Craggs <popey@debenclipper.com>
2021-09-23 17:32:43 +0100

    edit ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [{
         name: "scoreboard",
         script: "npm",
-        args: "start",
+        args: "start -p 8000",
         env_production: {
             NODE_ENV: "production"
         },
